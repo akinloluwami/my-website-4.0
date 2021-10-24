40e8e6ac6a3059d60a7bd6505d1d65371d0575b9
Akinkunmi <akinkunmiisrael41@gmail.com>
2021-10-24 15:14:56 +0100

    ...

--- akinkumi.js
+++ akinkumi.js
@@ -1,11 +1,11 @@
-// const preloader = document.querySelector(".preloader");
-// const main = document.querySelector(".main");
+const preloader = document.querySelector(".preloader");
+const main = document.querySelector(".main");
 
-// main.style.display = "none";
+main.style.display = "none";
 
-// window.addEventListener("load", () => {
-//   setTimeout(() => {
-//     preloader.style.display = "none";
-//     main.style.display = "block";
-//   }, 2000);
-// });
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    preloader.style.display = "none";
+    main.style.display = "block";
+  }, 2000);
+});
