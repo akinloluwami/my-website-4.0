1ff9b1e186bbcf14566675c2573ee3915c9e3f16
Akinkunmi <akinkunmiisrael41@gmail.com>
2021-10-24 01:09:50 +0100

    Initial commit

---
+++ akinkumi.js
@@ -0,0 +1,11 @@
+// const preloader = document.querySelector(".preloader");
+// const main = document.querySelector(".main");
+
+// main.style.display = "none";
+
+// window.addEventListener("load", () => {
+//   setTimeout(() => {
+//     preloader.style.display = "none";
+//     main.style.display = "block";
+//   }, 2000);
+// });
